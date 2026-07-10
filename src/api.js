a4f22e39c430cdd3134da98133827c38d1e0d0af
LYKA SK <binlyka6@gmail.com>
2026-07-10 09:54:40 +0700

    change port to 3000

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 // Base URL of the provided Event Ticket Booking API (keep the backend running!)
-export const BASE_URL = 'http://localhost:4000/api';
+export const BASE_URL = 'http://localhost:3000/api';
 
 // You may add your own fetch helper(s) here if you want.
 // Error responses from the API always look like: { "error": "message" }
